d74df4abc657c698a958af5ad65386dba979aec3
miguelMedinaCastro <miguelmedinadecastro@gmail.com>
2025-05-25 12:58:58 -0300

    commit

--- backend/server.js
+++ backend/server.js
@@ -56,7 +56,7 @@ app.post("/api/plan", async (req, res) => {
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 })
 
